c815887f32345ca894186fb4778d3d196a224636
Vilson1984 <vcmorion@hotmail.com>
2023-05-30 19:53:54 -0300

    App + form + controller Home + Controller apostas

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'apostas/create'
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "home#index"
+
+  post '/apostas', to:"apostas#create"
+end
