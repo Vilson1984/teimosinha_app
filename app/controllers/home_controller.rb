532d03fa8a62b724d88d3970866a0790ae4181f9
Vilson1984 <vcmorion@hotmail.com>
2023-06-01 11:37:44 -0300

    + sorteio pc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
     def index
-      @selected_numbers = params[:nums_aposta] || []
+      @aposta = params[:nums_aposta] || []
+
+      sorteio
     end  
+
+    def sorteio
+      @bilhete_premiado = []
+      
+      # SORTEIO NÚMEROS DO PC
+        @numeros_disp = (1..10)
+        @bilhete_premiado << @numeros_disp.sort.sample(3)
+    end
+
+ 
 end
