9a8b1829b4c09fadef12ed365cd66fdbf1e39c75
Vilson1984 <vcmorion@hotmail.com>
2023-06-03 10:44:36 -0300

    +show random results index + target_array pre set

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,25 @@
 class HomeController < ApplicationController
   def index
-    @target_array = params[:nums_array]&.map(&:to_i) || []
+    @contador = 0
+    @target_array = [1, 2, 3]
+    #@target_array = params[:nums_array] || []
+    #@target_array = params[:nums_array]&.map(&:to_i) || []
     @results = []
     
-    until @results.last == @target_array || @results.length >= 1000
-      random_array = (1..10).to_a.sample(3)
-      @results << random_array
+   # while @target_array != @random_array
+   #   @random_array = (1..10).to_a.sample(3)
+   #   @results << @random_array
+   # end
+ 
+   loop do 
+    @contador += 1
+    @random_array = (1..10).sort.to_a.sample(3)
+    @results << @random_array
+    
+    if @target_array == @random_array
+      break
     end
+   end
+
   end
 end
