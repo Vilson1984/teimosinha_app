0c584c448dd299bbf05f0939bba8118d28ded24a
Vilson1984 <vcmorion@hotmail.com>
2023-06-05 13:55:37 -0300

    +sorteio +contador_jogadas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,55 @@
 class HomeController < ApplicationController
   def index
-    @contador = 0
-    @target_array = [1, 2, 3]
+    target_array
+    random_array_compara
+
+    def initialize
+      @contador = 0 
+    end
     #@target_array = params[:nums_array] || []
     #@target_array = params[:nums_array]&.map(&:to_i) || []
-    @results = []
-    
+    #@results = []
+    #@contador = 0
    # while @target_array != @random_array
    #   @random_array = (1..10).to_a.sample(3)
    #   @results << @random_array
    # end
- 
-   loop do 
-    @contador += 1
-    @random_array = (1..10).sort.to_a.sample(3)
-    @results << @random_array
-    
-    if @target_array == @random_array
-      break
-    end
-   end
+
+
+  # loop do 
+  #  @contador += 1
+  #  @random_array = (1..10).to_a.sample(3)
+  #  @results << @random_array
+  #  
+  #  if @target_array == @random_array
+  #    break
+  #  end
+  # end
 
   end
+
+  def target_array
+    @target_array = [1, 2, 3]
+    #@nums_array = params[:nums_array].split(",")
+  end
+
+
+
+
+  def random_array_compara
+    @results = []
+    @contador = 0
+    loop do 
+        
+        @contador += 1
+        @random_array = (1..10).to_a.sample(3)
+        @results << @random_array
+        
+        if @target_array == @random_array
+          break
+        end
+       end
+  end
+
+
 end
